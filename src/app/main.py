30e69d904b7321e8f3e5140865c40c024ba0d745
mirzasyhm <msfalah02@gmail.com>
2025-01-28 10:49:43 +0900

    end of day 1

---
+++ src/app/main.py
@@ -0,0 +1,16 @@
+import uvicorn
+from fastapi import FastAPI
+
+from app.settings import Settings
+
+app = FastAPI()
+settings = Settings()
+
+
+def main():
+    uvicorn.run(
+        "app.main:app",
+        host=settings.host_address,
+        port=settings.port,
+        log_level="debug",
+    )
